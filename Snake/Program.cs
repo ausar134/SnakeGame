6e190efeaaaffdf43798e91e5b77f9d5b8892409
nikolas plytas <nikolaspl13@hotmail.com>
2019-11-21 19:33:20 +0200

    Extra things

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,25 +11,23 @@ namespace Snake
 
         {
             string myName = "Aggelos";
-            string mySurname = "Theofanous";
             int myAge = 23;
 
+            char[] exclamationPoint = new char[1] {'!'};
+            char point = '!';
             string playersName = "";
             int playersAge;
 
 
-            //Create a variable and add 1 more until the total is equal to 10...
-            //int number = 0;
-            //do
-            //{
-            //    Console.WriteLine("Hello, my name is " + myName + " " + mySurname + " and I am " + myAge + " years old");
-            //    number++;
-            //} while (number < 10);
-
             //Create a count that is equal to 0 and add 1 more until count reaches 10, the print my name...
             for (int count = 0; count < 10; count++)
             {
-                Console.WriteLine(myName);
+                Console.WriteLine(myName + point);
+
+                //for (int expoint = 0; expoint < exclamationPoint.Length; expoint++)
+                //{
+                //    Console.WriteLine(point);
+                //}    
             }
 
             Console.ReadLine();
@@ -41,6 +39,15 @@ namespace Snake
             playersAge = Console.Read();
             Console.WriteLine("So just to make sure, your name is " + playersName + " and you are " + playersAge);
 
+
+
+            //Create a variable and add 1 more until the total is equal to 10...
+            //int number = 0;
+            //do
+            //{
+            //    Console.WriteLine("Hello, my name is " + myName + " and I am " + myAge + " years old");
+            //    number++;
+            //} while (number < 10);
         }
     }
 }
