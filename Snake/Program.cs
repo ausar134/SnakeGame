d276ecf6e5c6a33fe2af4bc08b5dc42daa9a1e16
nikolas plytas <nikolaspl13@hotmail.com>
2019-11-22 17:06:50 +0200

    Endless Loop increase/decrease with 1 bug

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,40 +15,56 @@ namespace Snake
             bool increasingNumber = true;
             int pointNumber = 0;
 
-            //Cycle through 20 times... 
-            //We start from 0-19 it's used for increase and decrease...
-            for (int i = 0; i < 19; i++)
+            //Infinite loop of the Code Below...
+            while (true)
             {
-                //When it completes 9 cycles...
-                //Increase changes to false because of the trigger below...
-                if (increasingNumber)
-                {
-                    pointNumber += 1;
-                }
-                else
-                {
-                    pointNumber -= 1;
-                }
-                //Trigger to change from increase to decrease...
-                if (i == 9)
-                {
-                    increasingNumber = false;
-                }
-
-                //Reset the string each time...
-                pointString = "";
-
-                //Re-create and add the "!" N times
-                //depending on the value of pointNumber...
-                for (int j = 0; j < pointNumber; j++)
-                {
-                    pointString += "!";
-                }
-                Console.WriteLine(myName + pointString);
                 
-            }
-            Console.ReadLine();
+                //Cycle through 20 times... 
+                //We start from 0-19 it's used for increase and decrease...
+                for (int i = 0; i < 19; i++)
+                {
 
+                
+                    //When it completes 9 cycles...
+                    //Increase changes to false because of the trigger below...
+                    if (increasingNumber)
+                    {
+                        pointNumber += 1;
+                    }
+                    else
+                    {
+                        pointNumber -= 1;
+                    }
+
+                    //Trigger to change from increase to decrease...
+                    if (i == 9)
+                    {
+                        increasingNumber = false;
+
+                    }
+
+                    //Trigger to change from decrease to increase again...
+                    else if (i == 0)
+                    {
+                        increasingNumber = true;
+                    }
+
+                    //Reset the string each time...
+                    pointString = "";
+
+                    //Re-create and add the "!" N times
+                    //depending on the value of pointNumber...
+                    for (int j = 0; j < pointNumber; j++)
+                    {
+                        pointString += "!";
+                    }
+                    Console.WriteLine(myName + pointString);
+                    Console.ReadLine();
+
+                }
+            }
+                
+            
         }
     } 
 }
