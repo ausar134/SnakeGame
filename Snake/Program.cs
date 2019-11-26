da17a35b1340574186dfc5065656ee678959206c
nikolas plytas <nikolaspl13@hotmail.com>
2019-11-26 14:55:08 +0200

    Changed 8 if statements to switch statements, fixed a Draw bug

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -7,6 +7,10 @@ namespace Snake
 {
     public class Program
     {
+
+        private const int MaxHeight = 30;
+        private const int MaxWidth = 119;
+
         static void Main(string[] args)
         {
             int xPos = 60;
@@ -17,6 +21,7 @@ namespace Snake
             int origWidth;
             int origHeight;
             bool isGamePlayable = true;
+            Random randomFood = new Random();
 
             origWidth = Console.WindowWidth;
             origHeight = Console.WindowHeight;
@@ -26,75 +31,44 @@ namespace Snake
             Console.SetWindowSize(origWidth, origHeight);
             Console.CursorVisible = false;
             Console.Clear();
-            
-
+            Console.SetCursorPosition(xPos, yPos);
 
             while (isGamePlayable)
             {
                 Console.Clear();
-                Console.SetCursorPosition(xPos, yPos);
+                
 
-               
+
                 //xPos += numberX;
                 //yPos += numberY;
 
                 #region Keyboard Controls and Window Boundary Check
-
                 if (Console.KeyAvailable)
                 {
                     keyInfo = Console.ReadKey();
-                    if (keyInfo.Key == ConsoleKey.UpArrow && yPos !=0)
-                    {
-                        yPos -= 1;
 
-                        if(keyInfo.Key == ConsoleKey.DownArrow)
-                        {
-                            return;
-                        }
-                    }
-                    if (keyInfo.Key == ConsoleKey.LeftArrow && xPos !=0)
+                    switch (keyInfo.Key)
                     {
-                        xPos -= 1;
-
-                       if(keyInfo.Key == ConsoleKey.RightArrow)
-                       {
-                            return;
-                       }
-                    }
-                        if (keyInfo.Key == ConsoleKey.RightArrow && xPos !=119)
-                        {
-                        xPos += 1;
-                           
-                          if(keyInfo.Key == ConsoleKey.LeftArrow)
-                          {
-                            return;
-                          }
-                            
-                        }
-                    if(keyInfo.Key == ConsoleKey.DownArrow && yPos !=30)
-                    {
-                        yPos += 1;
-
-                        if (keyInfo.Key == ConsoleKey.UpArrow)
-                        {
-                            return;
-                        }
+                        case ConsoleKey.UpArrow when (yPos != 0):
+                            yPos -= 1;
+                            break;
+                        case ConsoleKey.DownArrow when (yPos != MaxHeight):
+                            yPos += 1;
+                            break;
+                        case ConsoleKey.RightArrow when (xPos != MaxWidth):
+                            xPos += 1;
+                            break;
+                        case ConsoleKey.LeftArrow when (xPos != 0):
+                            xPos -= 1;
+                            break;
+                        default:
+                            break;
                     }
                 }
-                #endregion 
+                #endregion
 
 
-                //if (xPos >= origWidth-1 || xPos == 0)
-                //{
-                //    numberX = numberX * (-1);
-                //    //xAsc = !xAsc;
-                //}
-                //if (yPos >= origHeight || yPos ==0)
-                //{
-                //    numberY = numberY * (-1);
-                //  //  yAsc = !yAsc;
-                //}
-                
+                Console.SetCursorPosition(xPos, yPos);
                 Console.Write("*");
                 Thread.Sleep(50);
             }
