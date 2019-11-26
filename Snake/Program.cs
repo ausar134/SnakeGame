9d868d15435549db5987f15914d1307d60e56c3c
nikolas plytas <nikolaspl13@hotmail.com>
2019-11-26 13:12:32 +0200

    Added keyboard movement controls and a new boundary check

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,22 +9,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int xPos = 0;
-            int yPos = 0;
-            int numberX = 1;
-            int numberY = 1;
-
-            
+            int xPos = 60;
+            int yPos = 15;
+            //int numberX = 0;
+            //int numberY = 0;
 
             int origWidth;
             int origHeight;
+
             origWidth = Console.WindowWidth;
             origHeight = Console.WindowHeight;
-      
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(); 
+
             Console.SetWindowSize(origWidth, origHeight);
             Console.CursorVisible = false;
             Console.Clear();
-
+            
 
 
             while (true)
@@ -33,20 +34,64 @@ namespace Snake
                 Console.SetCursorPosition(xPos, yPos);
 
                
-                xPos += numberX;
-                yPos += numberY;
+                //xPos += numberX;
+                //yPos += numberY;
 
-                if (xPos >= origWidth-1 || xPos == 0)
-                {
-                    numberX = numberX * (-1);
-                    //xAsc = !xAsc;
-                }
-                if (yPos >= origHeight || yPos ==0)
-                {
-                    numberY = numberY * (-1);
-                  //  yAsc = !yAsc;
-                }
+                #region Keyboard Controls and Boundary check
 
+                if (Console.KeyAvailable)
+                {
+                    keyInfo = Console.ReadKey();
+                    if (keyInfo.Key == ConsoleKey.UpArrow && yPos !=0)
+                    {
+                        yPos -= 1;
+
+                        if(keyInfo.Key == ConsoleKey.DownArrow)
+                        {
+                            return;
+                        }
+                    }
+                    if (keyInfo.Key == ConsoleKey.LeftArrow && xPos !=0)
+                    {
+                        xPos -= 1;
+
+
+                       if(keyInfo.Key == ConsoleKey.RightArrow)
+                        {
+                            return;
+                        }
+                    }
+                    
+                        if (keyInfo.Key == ConsoleKey.RightArrow && xPos !=119)
+                        {
+                        xPos += 1;
+                           
+                          if(keyInfo.Key == ConsoleKey.LeftArrow)
+                        { return; }
+                            
+                        }
+                    if(keyInfo.Key == ConsoleKey.DownArrow && yPos !=30)
+                    {
+                        yPos += 1;
+
+                        if (keyInfo.Key == ConsoleKey.UpArrow)
+                        { return; }
+                    }
+                }
+                #endregion 
+
+
+                //if (xPos >= origWidth-1 || xPos == 0)
+                //{
+                //    numberX = numberX * (-1);
+                //    //xAsc = !xAsc;
+                //}
+                //if (yPos >= origHeight || yPos ==0)
+                //{
+                //    numberY = numberY * (-1);
+                //  //  yAsc = !yAsc;
+                //}
+                
                 Console.Write("*");
                 Thread.Sleep(50);
             }
