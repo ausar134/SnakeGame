dc3d731ad2af9fe2f451d209f558b2ab3c9291a9
nikolas plytas <nikolaspl13@hotmail.com>
2019-12-03 18:35:31 +0200

    Added life+score and fixed logic to work correctly with those

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,33 +13,39 @@ namespace Snake
     {
         private static Random Random = new Random();
 
-        private static int MaxHeight = 30;
-        private static int MaxWidth = 119;
+        private const string TopLeftCorner = "╔";
 
-        private static int BorderSize = 4;
+        private const int MaxHeight = 30;
+        private const int MaxWidth = 119;
+
+        private const int BorderSize = 4;
 
         private const int StartingX = 60;
         private const int StartingY = 15;
 
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.CursorVisible = false;
-                Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+            Console.OutputEncoding = Encoding.UTF8;
 
-                SelfMovingSnake();
-                Environment.Exit(0);
-
-            }
+            DrawBorders();
+            SelfMovingSnake();
+            //#if (Debug)
+            //Console.ReadLine();
+            //#endif
         }
 
-        #region Game Logic 
-        private static void SelfMovingSnake()
+        static void InitGame()
         {
-            int BorderHeight = MaxHeight-BorderSize;
-            int BorderWidth = MaxWidth-BorderSize;
+            
+        }
 
+        static void DrawBorders()
+        {
+            var color = Console.ForegroundColor;
+            var x = Console.CursorTop;
+            var y = Console.CursorLeft;
+            Console.ForegroundColor = ConsoleColor.White;
             for (int i = BorderSize; i <= MaxHeight - BorderSize; i++)
             {
                 Console.SetCursorPosition(BorderSize, i);
@@ -48,7 +54,7 @@ namespace Snake
                 Console.Write("║");
             }
 
-            for (int j = BorderSize; j <= MaxWidth-BorderSize; j++)
+            for (int j = BorderSize; j <= MaxWidth - BorderSize; j++)
             {
                 Console.SetCursorPosition(j, BorderSize);
                 Console.Write("═");
@@ -57,7 +63,7 @@ namespace Snake
             }
 
             Console.SetCursorPosition(BorderSize, BorderSize);
-            Console.Write("╔");
+            Console.Write(TopLeftCorner);
 
             Console.SetCursorPosition(MaxWidth - BorderSize, BorderSize);
             Console.Write("╗");
@@ -67,6 +73,30 @@ namespace Snake
 
             Console.SetCursorPosition(MaxWidth - BorderSize, MaxHeight - BorderSize);
             Console.Write("╝");
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+        }
+
+        #region Game Logic 
+        private static void SelfMovingSnake()
+        {
+            int BorderHeight = MaxHeight-BorderSize;
+            int BorderWidth = MaxWidth-BorderSize;
+            
+            int lifePosX = BorderSize - 3;
+            int lifePosY = BorderSize - 3;
+            int startingLives = 5;
+
+            Console.SetCursorPosition(lifePosX, lifePosY);
+            Console.Write("Remaining Lives : " + startingLives);
+
+            int foodScoreX = MaxWidth - 12;
+            int foodScoreY = MaxHeight - 29;
+            int foodScore = 0;
+            int resetSpeed = 500;
+
+            Console.SetCursorPosition(foodScoreX, foodScoreY);
+            Console.Write("Score : " + foodScore);
 
             int foodX = Random.Next(BorderSize + 2, BorderWidth-2);
             int foodY = Random.Next(BorderSize + 2, BorderHeight-2);
@@ -82,6 +112,7 @@ namespace Snake
             {
                 snakeBody[i] = new Point(0, 0);
             }
+
             Console.SetCursorPosition(foodX, foodY);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("*");
@@ -158,6 +189,10 @@ namespace Snake
                     {
                         snakeSpeed /= 2;
                     }
+
+                    foodScore += 1;
+                    Console.SetCursorPosition(foodScoreX, foodScoreY);
+                    Console.Write("Score : " + foodScore);
                     
                     Console.SetCursorPosition(foodX, foodY);
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -169,12 +204,71 @@ namespace Snake
                 //Win Condition...
                 if (snakeSegments == snakeBody.Length - 1)
                 {
+
                     Console.SetCursorPosition(StartingX - 15, StartingY);
                     Console.WriteLine("Congratulations!!");
                     Console.SetCursorPosition(StartingX - 25, StartingY + 1);
                     Console.WriteLine("You cleared the first level!");
                     Console.SetCursorPosition(StartingX - 30, StartingY + 2);
-                    Console.WriteLine("Press enter to move on to the next stage");
+                    Console.WriteLine("The game will now quit..");
+                    Console.ReadLine();
+                    Console.Clear();
+
+#region Border Draw
+                    for (int i = BorderSize; i <= MaxHeight - BorderSize; i++)
+                    {
+                        Console.SetCursorPosition(BorderSize, i);
+                        Console.Write("║");
+                        Console.SetCursorPosition(MaxWidth - BorderSize, i);
+                        Console.Write("║");
+                    }
+
+                    for (int j = BorderSize; j <= MaxWidth - BorderSize; j++)
+                    {
+                        Console.SetCursorPosition(j, BorderSize);
+                        Console.Write("═");
+                        Console.SetCursorPosition(j, MaxHeight - BorderSize);
+                        Console.Write("═");
+                    }
+
+                    Console.SetCursorPosition(BorderSize, BorderSize);
+                    Console.Write("╔");
+
+                    Console.SetCursorPosition(MaxWidth - BorderSize, BorderSize);
+                    Console.Write("╗");
+
+                    Console.SetCursorPosition(BorderSize, MaxHeight - BorderSize);
+                    Console.Write("╚");
+
+                    Console.SetCursorPosition(MaxWidth - BorderSize, MaxHeight - BorderSize);
+                    Console.Write("╝");
+#endregion
+
+                    foodX = Random.Next(BorderSize + 2, BorderWidth - 2);
+                    foodY = Random.Next(BorderSize + 2, BorderHeight - 2);
+
+                    key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.SetCursorPosition(StartingX, StartingY);
+                        Console.WriteLine("Bye!");
+                        break;
+                    }
+
+                    for (int i = 0; i < snakeBody.Length; i++)
+                    {
+                        snakeBody[i].X = 0;
+                        snakeBody[i].Y = 0;
+                    }
+
+                    foodScore = 0;
+                    snakeSegments = 1;
+                    snakeBody[0].X = StartingX;
+                    snakeBody[0].Y = StartingY;
+                    collision = false;
+                    xStep = 1;
+                    yStep = 0;
+
                 }
 
                 //Check for collision with snakebody...
@@ -191,14 +285,15 @@ namespace Snake
 
                 //Lose condition...
                 if (collision || snakeBody[0].X >= BorderWidth || snakeBody[0].X <= BorderSize 
-                    || snakeBody[0].Y >= BorderHeight || snakeBody[0].Y <= BorderSize)
+                    || snakeBody[0].Y >= BorderHeight || snakeBody[0].Y <= BorderSize || startingLives==0)
                 {
-                    foodX = Random.Next(BorderSize+2, BorderWidth-2);
-                    foodY = Random.Next(BorderSize+2, BorderHeight-2);
-                    Console.SetCursorPosition(StartingX - 10, StartingY - 15);
-                    Console.WriteLine("You lose... Press enter to restart.");
+
+                    foodX = Random.Next(BorderSize + 2, BorderWidth - 2);
+                    foodY = Random.Next(BorderSize + 2, BorderHeight - 2);
+                    Console.SetCursorPosition(StartingX - 15, StartingY);
+                    Console.WriteLine("You lost.. Press enter to quit.");
                     key = Console.ReadKey();
-                    if (key.Key != ConsoleKey.Enter)
+                    if (key.Key == ConsoleKey.Enter)
                     {
                         Console.SetCursorPosition(StartingX, StartingY);
                         Console.WriteLine("Bye!");
@@ -210,20 +305,88 @@ namespace Snake
                         snakeBody[i].X = 0;
                         snakeBody[i].Y = 0;
                     }
-                    
+
+                    for (int j = startingLives; j <= 5; ) 
+                    {
+                            startingLives -= 1;
+                            Console.SetCursorPosition(lifePosX, lifePosY);
+                            Console.Write("Remaining Lives : " + startingLives);
+                            break;
+                    }
+
+                    //Restart and Redraw...
+                    if (key.Key != ConsoleKey.Enter)
+                    {
+                        Console.Clear();
+                        snakeSpeed = resetSpeed;
+
+                        Console.SetCursorPosition(foodScoreX, foodScoreY);
+                        Console.Write("Score : " + foodScore);
+                        Console.SetCursorPosition(lifePosX, lifePosY);
+                        Console.Write("Remaining Lives : " + startingLives);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        foodX = Random.Next(BorderSize + 2, BorderWidth - 2);
+                        foodY = Random.Next(BorderSize + 2, BorderHeight - 2);
+                        Console.SetCursorPosition(foodX, foodY);
+                        Console.Write("*");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+#region Border Draw
+                        for (int i = BorderSize; i <= MaxHeight - BorderSize; i++)
+                        {
+                            Console.SetCursorPosition(BorderSize, i);
+                            Console.Write("║");
+                            Console.SetCursorPosition(MaxWidth - BorderSize, i);
+                            Console.Write("║");
+                        }
+
+                        for (int j = BorderSize; j <= MaxWidth - BorderSize; j++)
+                        {
+                            Console.SetCursorPosition(j, BorderSize);
+                            Console.Write("═");
+                            Console.SetCursorPosition(j, MaxHeight - BorderSize);
+                            Console.Write("═");
+                        }
+
+                        Console.SetCursorPosition(BorderSize, BorderSize);
+                        Console.Write("╔");
+
+                        Console.SetCursorPosition(MaxWidth - BorderSize, BorderSize);
+                        Console.Write("╗");
+
+                        Console.SetCursorPosition(BorderSize, MaxHeight - BorderSize);
+                        Console.Write("╚");
+
+                        Console.SetCursorPosition(MaxWidth - BorderSize, MaxHeight - BorderSize);
+                        Console.Write("╝");
+#endregion
+                    }
+
+                    if (startingLives == 0)
+                    {
+                        Console.SetCursorPosition(StartingX - 20, StartingY);
+                        Console.Write("Too bad.. You don't have any lives left..");
+                        Console.SetCursorPosition(StartingX - 20, StartingY + 1);
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write("The game will quit unless you press the Enter key..");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
                     snakeSegments = 1;
                     snakeBody[0].X = StartingX;
                     snakeBody[0].Y = StartingY;
                     collision = false;
                     xStep = 1;
                     yStep = 0;
-
                 }
+
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(snakeSpeed);
+                    Thread.Sleep(snakeSpeed/2);
             }
-            #endregion
-        } 
+        }
+#endregion
+
+       
     }
 }
 
